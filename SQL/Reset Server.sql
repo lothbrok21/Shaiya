571060c26fd1d542abac68bf656c1df27e83d7c7
Lothbrok <67364485+lothbrok21@users.noreply.github.com>
2021-03-07 00:52:15 +0300

    Add files via upload

---
+++ SQL/Reset Server.sql
@@ -0,0 +1,16 @@
+USE PS_GameData
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_GameLog
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_UserData
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_Chatlog
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_Billing
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_GMTool
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE PS_STATICS
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
+USE WebResource
+EXEC sp_MSForEachTable 'TRUNCATE TABLE ?'
