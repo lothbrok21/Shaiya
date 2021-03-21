d1d31be8aacd8cf8dc7640b498f3c49376855c48
Lothbrok <67364485+lothbrok21@users.noreply.github.com>
2021-03-21 10:34:13 +0300

    Delete Remove Apostrophes.sql

--- Remove Apostrophes.sql
+++
@@ -1,5 +0,0 @@
-USE PS_GameDefs
---removes apostrophes from these columns to help prevent failed log inserts
-UPDATE dbo.Skills set SkillName=replace(SkillName,'''','');
-UPDATE dbo.Mobs set MobName=replace(MobName,'''','');
-UPDATE dbo.Items set ItemName=replace(ItemName,'''','');
