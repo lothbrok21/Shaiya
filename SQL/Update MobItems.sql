708e6790e4fa261ab938dc6c3871ef99ca64b011
Lothbrok <67364485+lothbrok21@users.noreply.github.com>
2023-03-28 12:58:05 +0300

    Update Update MobItems.sql

--- SQL/Update MobItems.sql
+++ SQL/Update MobItems.sql
@@ -1,2 +1,2 @@
-UPDATE PS_GameDefs.dbo.MobItems Set Grade = '',DropRate = '' WHERE MobID = '' AND ItemOrder = '' -- One by one
-UPDATE PS_GameDefs.dbo.MobItems Set ItemOrder = '',Grade = '',DropRate = '' WHERE MobID BETWEEN '' AND '' -- In a range
+UPDATE PS_GameDefs.dbo.MobItems SET Grade = '',DropRate = '' WHERE MobID = '' AND ItemOrder = '' -- One by one
+UPDATE PS_GameDefs.dbo.MobItems Grade = '',DropRate = '' WHERE MobID BETWEEN '' AND '' AND ItemOrder = '' -- In a range
